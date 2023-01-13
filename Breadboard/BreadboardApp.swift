3dfeda4b533e33e2864c858371d86b77ac730cda
Alexander Obenauer <alex@mindsense.co>
2023-01-13 01:13:05 -0500

    Initial Commit

---
+++ Breadboard/BreadboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreadboardApp.swift
+//  Breadboard
+//
+//  Created by Alexander Obenauer on 1/13/23.
+//
+
+import SwiftUI
+
+@main
+struct BreadboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BreadboardUITests/BreadboardUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  BreadboardUITestsLaunchTests.swift
+//  BreadboardUITests
+//
+//  Created by Alexander Obenauer on 1/13/23.
+//
+
+import XCTest
+
+final class BreadboardUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
